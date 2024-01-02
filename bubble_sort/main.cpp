740323ab71e5a9d7972f745b152855b2c85445b6
abu-hiba <tbolton96@icloud.com>
2024-01-02 17:30:22 +0000

    optimize bubble sort by looping over smaller section of array than while loop

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -6,15 +6,19 @@ void bubbleSort(int arr[], int size) {
     int endIndex = size - 1;
     bool swapped = true;
 
-    while (swapped) {
+    for (int i = 0; i < size - 1; i++) {
         swapped = false;
-        for (int i = 0; i < endIndex; i++) {
-            if (arr[i] > arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
                 swapped = true;
             }
+        }
+
+        if (!swapped) {
+            break;
         }
     }
 }
