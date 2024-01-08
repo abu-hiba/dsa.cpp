4079aa8b9f6eed6eb80ab4eed39ef63052877d8b
abu-hiba <tbolton96@icloud.com>
2024-01-08 22:00:00 +0000

    Add chapter 6 exercise 4

---
+++ exercises/stringContainsX/solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+bool containsX(std::string str) {
+    bool foundX = false;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'X') {
+            foundX = true;
+        }
+    }
+
+    return foundX;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::printf("Specify a single string to search e.g.:\n\n %s eXample\nor\n %s \"example string\"\n", argv[0], argv[0]);
+        return 1;
+    }
+
+    std::string stringToSearch = argv[1];
+
+    bool result = containsX(stringToSearch);
+
+    if (result == 1) {
+        std::cout << "true" << std::endl;
+    } else if (result == 0) {
+        std::cout << "false" << std::endl;
+    }
+
+    return 0;
+}
+
