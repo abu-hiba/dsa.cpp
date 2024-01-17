e6f83e5018eb3d6780eee4daee7558026701fbc5
abu-hiba <tbolton96@icloud.com>
2024-01-17 15:21:39 +0000

    First solution to ch. 8 ex. 1

---
+++ exercises/arrayIntersection/solution.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+
+std::vector<int> intersection(std::vector<int> vec1, std::vector<int> vec2) {
+    std::vector<int> smallerVector;
+    std::vector<int> largerVector;
+    std::map<int, bool> map;
+    std::vector<int> vecIntersection;
+
+    if (vec1.size() > vec2.size()) {
+        smallerVector = vec2;
+        largerVector = vec1;
+    } else {
+        smallerVector = vec1;
+        largerVector = vec2;
+    }
+
+    for (int value : smallerVector) {
+        map[value] = true;
+    }
+
+    for (int value : largerVector) {
+        if (map[value]) {
+            vecIntersection.push_back(value);
+        }
+    }
+
+    return vecIntersection;
+}
+
+void printVector(std::vector<int> vec) {
+    for (int value : vec) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> readVector(std::string message) {
+    int input;
+    std::vector<int> inputVector;
+
+    std::cout << message << std::endl;
+
+    while (std::cin >> input) {
+        inputVector.push_back(input);
+    }
+
+    return inputVector;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<int> vector1 =
+        readVector("Enter a list of integers followed by any non-integer "
+                   "character e.g.: \n1 2 3 4 5;");
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << std::endl;
+
+    std::vector<int> vector2 =
+        readVector("Enter a second list of integers followed by any "
+                   "non-integer character e.g.: \n0 2 4 6 8;");
+
+    std::vector<int> result = intersection(vector1, vector2);
+    std::cout << std::endl << "Intersection:" << std::endl;
+    printVector(result);
+
+    return 0;
+}
