a49d6b00885809faa2834ed08059a9eda51e7d40
abu-hiba <tbolton96@icloud.com>
2024-01-17 19:15:23 +0000

    Format last solution

--- exercises/duplicateStrings/solution.cpp
+++ exercises/duplicateStrings/solution.cpp
@@ -10,7 +10,7 @@ std::string findDuplicate(std::vector<std::string> strings) {
         if (map[s]) {
             return s;
         } else {
-            map[s] = true; 
+            map[s] = true;
         }
     }
 
@@ -19,8 +19,9 @@ std::string findDuplicate(std::vector<std::string> strings) {
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        std::printf("Specify a list of strings e.g.:\n\n %s abc def ghi def jkl\n",
-                    argv[0]);
+        std::printf(
+            "Specify a list of strings e.g.:\n\n %s abc def ghi def jkl\n",
+            argv[0]);
         return 1;
     }
 
