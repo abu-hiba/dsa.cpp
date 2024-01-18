452c09cb5a962fa97236ccdcffcddc92dd784706
abu-hiba <tbolton96@icloud.com>
2024-01-18 18:58:45 +0000

    Question and solution to ch.8 ex.3

---
+++ exercises/missingLetter/solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+char missingLetter(std::string str) {
+    std::map<char, bool> testChars;
+
+    for (char c : str) {
+        testChars[c] = true;
+    }
+
+    for (char l = 'a'; l <= 'z'; l++) {
+        if (testChars[l] != true) {
+            return l;
+        }
+    }
+
+    return '_';
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::printf("Specify a single string to search e.g.:\n\n %s "
+                    "\"the quick brown box jumps over a lazy dog\"\n",
+                    argv[0]);
+        return 1;
+    }
+
+    std::string stringToSearch = argv[1];
+
+    char result = missingLetter(stringToSearch);
+    std::cout << "Missing letter: " << result << std::endl;
+
+    return 0;
+}
