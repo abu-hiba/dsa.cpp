3e9aa7412577ab8f44e285317e9a3226dfc92330
abu-hiba <tbolton96@icloud.com>
2024-01-22 17:46:22 +0000

    Adds array sub set example using hash table

---
+++ hash_tables/examples/array_subset/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+
+bool isSubset(std::vector<int> vec1, std::vector<int> vec2) {
+    std::vector<int> largerVec;
+    std::vector<int> smallerVec;
+    std::map<int, bool> map;
+
+    if (vec1.size() > vec2.size()) {
+        largerVec = vec1;
+        smallerVec = vec2;
+    } else {
+        largerVec = vec2;
+        smallerVec = vec1;
+    }
+
+    for (int i = 0; i < smallerVec.size(); i++) {
+        bool foundMatch = false;
+
+        for (int j = 0; j < largerVec.size(); j++) {
+            if (smallerVec[i] == largerVec[j]) {
+                foundMatch = true;
+                break;
+            }
+        }
+
+        if (foundMatch == false) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::vector<int> readVector(std::string message) {
+    int input;
+    std::vector<int> inputVector;
+
+    std::cout << message << std::endl;
+
+    while (std::cin >> input) {
+        inputVector.push_back(input);
+    }
+
+    return inputVector;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<int> vector1 =
+        readVector("Enter a list of integers followed by any non-integer "
+                   "character e.g.: \n1 2 3 4 5;");
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << std::endl;
+
+    std::vector<int> vector2 =
+        readVector("Enter a second list of integers");
+
+    bool result = isSubset(vector1, vector2);
+    if (result == 0) {
+        std::cout << std::endl << "false" << std::endl;
+    } else {
+        std::cout << std::endl << "true" << std::endl;
+    }
+
+    return 0;
+}
