7a79ebfba93e86bf80424a4c59f290c1cfcc7609
tombolton96 <tbolton96@icloud.com>
2024-02-07 23:07:24 +0000

    Adds Stack constrcutors and tries some examples

--- stacks/stack.cpp
+++ stacks/stack.cpp
@@ -6,8 +6,8 @@ template <typename T> class Stack {
     std::vector<T> data = {};
 
   public:
-    // Stack() {}
-    // Stack(std::vector<T> vec) { this->data = vec; }
+    Stack() {}
+    Stack(std::vector<T> vec) { this->data = vec; }
 
     void push(T element) { this->data.push_back(element); }
 
@@ -21,16 +21,28 @@ template <typename T> class Stack {
 };
 
 int main(int argc, char *argv[]) {
-    Stack<int> s = Stack<int>();
+    Stack<int> s = Stack<int>({ 1, 2, 3 });
+    Stack<int> s1 = Stack<int>();
+    Stack<std::string> s2 = Stack<std::string>();
 
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(20);
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.push(20);
+
+    s2.push("hello");
+    s2.push("world");
+    s2.push("abc");
+    std::cout << s2.pop() << std::endl;
+    std::cout << s2.read() << std::endl;
 
     std::cout << s.read() << std::endl;
     std::cout << s.pop() << std::endl;
     std::cout << s.read() << std::endl;
+
+    std::cout << s1.read() << std::endl;
+    std::cout << s1.pop() << std::endl;
+    std::cout << s1.read() << std::endl;
 
     return 0;
 }
