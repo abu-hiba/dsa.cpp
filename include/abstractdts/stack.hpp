1021ceb3a2f21f92845dc988aecf69a64672b21b
abu-hiba <tbolton96@icloud.com>
2024-02-08 09:21:36 +0000

    Move Stack class to own file

---
+++ include/abstractdts/stack.hpp
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <vector>
+template <typename T> class Stack {
+    std::vector<T> data = {};
+
+  public:
+    Stack() {}
+    Stack(std::vector<T> vec) { this->data = vec; }
+
+    void push(T element) { this->data.push_back(element); }
+
+    T pop() {
+        T element = this->data.back();
+        this->data.pop_back();
+        return element;
+    }
+
+    T read() { return this->data.back(); }
+};
+
+#endif
